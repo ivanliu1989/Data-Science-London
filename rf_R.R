b19a4541063587b2909a85ba7a4444d84ace1372
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-21 23:19:29 +1000

    R

--- rf_R.R
+++ rf_R.R
@@ -7,16 +7,19 @@ y <- read.csv("Data/trainLabels.csv", header= F)
 names(y)<- 'result'
 x <- cbind(x,y)
 str(x)
-# x$result <- as.factor(x$result)
+x$result <- as.factor(x$result)
 index <- createDataPartition(x$result, p = 0.8,list = F)
 x_train <- x[index,]
 x_test <- x[-index,]
 
 ###### pre process #######################################
 Grid1 <- expand.grid(mtry=c(5,20,40),n.trees=c(1000))
-fitControl1 <- trainControl(method="repeatedcv",10,10)
+fitControl1 <- trainControl(method="repeatedcv",10,10,
+                            classProbs = TRUE, allowParallel = TRUE,
+                            summaryFunction = twoClassSummary)
 set.seed(888)
-fit1 <- train(result~., method='rf',data=x,trControl = fitControl,tuneGrid = Grid)
+fit1 <- train(result~., method='rf',data=x,trControl = fitControl,
+              tuneGrid = Grid, importance = TRUE, metric = "ROC")
 pred1 <- predict(fit1, x_train)
 # index2 <- pred1 >0.5
 # index3 <- pred1 <0.5
@@ -39,8 +42,31 @@ Id <- c(1:length(pred_test))
 submission <- data.frame(Id, pred_test)
 names(submission)<-c("Id","Solution")
 write.table(submission, 'submission_rf_1.csv',sep=',',row.names = F)
-################ gbm ###########################
-Grid2 <- expand.grid(n.trees=c(100,500,1000),shrinkage=.1,interaction.depth=c(22))
+
+################ gbm ###################################################################
+Grid2 <- expand.grid(n.trees=c(100,500,1000),shrinkage=.1,interaction.depth=c(10,22,50))
 fitControl1 <- trainControl(method="repeatedcv",10,10)
 fit2 <- train(result~., method='gbm', data=x_train,
-              trControl = fitControl1,tuneGrid = Grid1,metric="ROC",verbose=T)
+              trControl = fitControl1,tuneGrid = Grid2,verbose=T)
+pred3 <- predict(fit2, x_train)
+# index2 <- pred1 >0.5
+# index3 <- pred1 <0.5
+# pred1[index2] <- 1
+# pred1[index3] <- 0
+confusionMatrix(pred3, x_train$result)
+
+############### SVM ####################################################################
+Grid3 <- expand.grid()
+fitControl3 <- trainControl(method="repeatedcv",10,10)
+fit3 <- train(result~., method='svm', data=x_train,
+              trControl = fitControl3,tuneGrid = Grid3,verbose=T)
+
+
+
+
+
+############### glm ###################################################################
+Grid4 <- expand.grid()
+fitControl4 <- trainControl(method="repeatedcv",10,10)
+fit4 <- train(result~., method='glm', family='binomial', data=x_train,
+              trControl = fitControl4,tuneGrid = Grid4,verbose=T)
