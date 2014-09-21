01c0095d48bda862d543535143059f1780e90538
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-21 19:31:22 +1000

    load data

--- dsl.py
+++ dsl.py
@@ -6,3 +6,7 @@ from sklearn.svm import SVC
 from sklearn.mixture import GMM
 from sklearn.base import BaseEstimator
 import matplotlib.pyplot as plt
+
+X_test = pd.read_csv('Data/test.csv', header=None).as_matrix()
+y = pd.read_csv('Data/trainLabels.csv', header=None)[0].as_matrix()
+X = pd.read_csv('Data/train.csv', header=None).as_matrix()
