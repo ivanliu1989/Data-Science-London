3e3632267e9e6fcd6b5701bc616836e443aef94b
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-21 23:02:04 +1000

    R

--- rf_R.R
+++ rf_R.R
@@ -7,14 +7,40 @@ y <- read.csv("Data/trainLabels.csv", header= F)
 names(y)<- 'result'
 x <- cbind(x,y)
 str(x)
-x$result <- as.factor(x$result)
+# x$result <- as.factor(x$result)
 index <- createDataPartition(x$result, p = 0.8,list = F)
 x_train <- x[index,]
 x_test <- x[-index,]
 
 ###### pre process #######################################
-Grid1 <- expand.grid(mtry=c(5,20,50))
-fitControl1 <- trainControl(method="cv",10)
+Grid1 <- expand.grid(mtry=c(5,20,40),n.trees=c(1000))
+fitControl1 <- trainControl(method="repeatedcv",10,10)
 set.seed(888)
-fit1 <- train(result~., method='rf',data=x_train,trControl = fitControl,tuneGrid = Grid)
-Grid1 <- expand.grid(n.trees=c(100,500,1000),shrinkage=.1,mtry=c(5,20,50))
+fit1 <- train(result~., method='rf',data=x,trControl = fitControl,tuneGrid = Grid)
+pred1 <- predict(fit1, x_train)
+# index2 <- pred1 >0.5
+# index3 <- pred1 <0.5
+# pred1[index2] <- 1
+# pred1[index3] <- 0
+confusionMatrix(pred1, x_train$result)
+pred2 <- predict(fit1, x_test)
+# index2 <- pred2 >.5
+# index3 <- pred2 <.5
+# pred2[index2] <- 1
+# pred2[index3] <- 0
+confusionMatrix(pred2, x_test$result)
+
+pred_test <- predict(fit1, test)
+index2 <- pred_test >.5
+index3 <- pred_test <.5
+pred_test[index2] <- 1
+pred_test[index3] <- 0
+Id <- c(1:length(pred_test))
+submission <- data.frame(Id, pred_test)
+names(submission)<-c("Id","Solution")
+write.table(submission, 'submission_rf_1.csv',sep=',',row.names = F)
+################ gbm ###########################
+Grid2 <- expand.grid(n.trees=c(100,500,1000),shrinkage=.1,interaction.depth=c(22))
+fitControl1 <- trainControl(method="repeatedcv",10,10)
+fit2 <- train(result~., method='gbm', data=x_train,
+              trControl = fitControl1,tuneGrid = Grid1,metric="ROC",verbose=T)
