b5e7cd11d4f80b8075a78a1648d791929b50a61f
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-21 22:39:16 +1000

    R

--- rf_R.R
+++ rf_R.R
@@ -13,3 +13,8 @@ x_train <- x[index,]
 x_test <- x[-index,]
 
 ###### pre process #######################################
+Grid1 <- expand.grid(mtry=c(5,20,50))
+fitControl1 <- trainControl(method="cv",10)
+set.seed(888)
+fit1 <- train(result~., method='rf',data=x_train,trControl = fitControl,tuneGrid = Grid)
+Grid1 <- expand.grid(n.trees=c(100,500,1000),shrinkage=.1,mtry=c(5,20,50))
