d0a368b1e262d7f6ace706c3035f4c3dd1c0146d
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-22 14:38:20 +1000

    gbm model

--- 22Sep2014_SVM.R
+++ 22Sep2014_SVM.R
@@ -27,6 +27,11 @@
     # none zero variables
     nzv <- nearZeroVar(x, saveMetrics = TRUE)
     nzv
+    # centering and scaling
+#     preProcValues <- preProcess(x, method = c("center", "scale"))
+#     x <- predict(preProcValues,x)
+    # transforming predictors
+
 
 ##### Split Data #####
     index <- createDataPartition(x$result, p = 0.8,list = F)
@@ -34,9 +39,32 @@
     x_test <- x[-index,]
 
 ##### Tune Parameters #####
-
+    fitControl <- trainControl(method = "repeatedcv",
+        number = 10,repeats = 10, summaryFunction = twoClassSummary,
+        classProbs = TRUE)
+    gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9),
+                            n.trees = (1:30)*50,
+                            shrinkage = 0.1)
 ##### Modeling #####
+    set.seed(825)
+    gbmFit <- train(result ~ ., data = x_train,
+                     method = "gbm",
+                     trControl = fitControl,
+                     verbose = FALSE,
+                     tuneGrid = gbmGrid,
+                     preProc = c('pca'),
+                     metric = "ROC")
 
+##### Evaluation #####
+    trellis.par.set(caretTheme())
+    png('tune_plot.png')    
+    plot(gbmFit, scales = list(x = list(log = 2)))
+    dev.off()
+    gbmImp <- varImp(gbmFit, scale = FALSE)
+    png('varImp.png')    
+    plot(gbmImp, top = 40)
+    dev.off()
+    getTrainPerf(gbmFit)
 ##### Prediction #####
 
 ##### Evaluation #####
