08ae2533bb5fa924ba69ca834f5e1b7dfeac37b8
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-22 12:55:03 +1000

    new model

--- 22Sep2014_SVM.R
+++ 22Sep2014_SVM.R
@@ -14,6 +14,19 @@
     x$result <- as.factor(x$result)
     levels(x$result) <- c('NO', 'YES')
     mean(is.na(x))
+    png("featurePlot.png",1024,1024)
+    featurePlot(x = x[, 1:40],
+            y = x$result,
+            plot = "box",
+            ## Pass in options to bwplot() 
+            scales = list(y = list(relation="free"),
+                          x = list(rot = 90)),
+            layout = c(4,10),
+            auto.key = list(columns = 2))
+    dev.off()
+    # none zero variables
+    nzv <- nearZeroVar(x, saveMetrics = TRUE)
+    nzv
 
 ##### Split Data #####
     index <- createDataPartition(x$result, p = 0.8,list = F)
