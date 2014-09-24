bd6da6ac991a4e4b33de1351809fc6558c443634
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-24 15:53:09 +1000

    svm_RadialCost

--- svm.R
+++ svm.R
@@ -10,6 +10,7 @@ names(y)<- 'result'
 x <- cbind(x,y)
 str(x)
 x$result <- as.factor(x$result)
+levels(x$result) <- c('No','Yes')
 mean(is.na(x))
 index <- createDataPartition(x$result, p = 0.8,list = F)
 x_train <- x[index,]
@@ -32,8 +33,14 @@ res <- gmm(g2~g1, x1)
     # gamma=c(0.01,0.001,0.0001)1/number of features
     # range=10^c(-3:3)
     # kernel=rbf
-Grid <- expand.grid(C=c(0.01,1,10,100,1000),gamma=c(0.01,0.001,0.0001),range=10^c(-3:3)) 
-fitControl <- trainControl(method="repeatedcv",10,5,classProbs = T)
-fit_svmLinear <- train(result~., method='svmRadialCost', data=x_train,
+Grid <- expand.grid(C=c(0.01,0.1,1,2,10,100,1000)) 
+fitControl <- trainControl(method="repeatedcv",10,10,classProbs = T)
+fit_svmRadialCost <- train(result~., method='svmRadialCost', data=x_train,
               trControl = fitControl, verbose=T, preProcess=c('pca'),
-              metric='ROC',tuneGrid=Grid)
+              tuneGrid=Grid)
+Pred <- predict(fit_svmRadialCost, x_train)
+confusionMatrix(Pred, x_train$result)
+
+##### Prediction #####
+Pred <- predict(fit_svmRadialCost, x_test)
+confusionMatrix(Pred, x_test$result)
