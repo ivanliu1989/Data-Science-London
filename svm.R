d708239cf4348a9d04dfb000bdb4cd343e81c40c
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-24 16:25:57 +1000

    svm_RadialCost_submission

--- svm.R
+++ svm.R
@@ -33,14 +33,24 @@ res <- gmm(g2~g1, x1)
     # gamma=c(0.01,0.001,0.0001)1/number of features
     # range=10^c(-3:3)
     # kernel=rbf
-Grid <- expand.grid(C=c(0.01,0.1,1,2,10,100,1000)) 
+Grid <- expand.grid(C=c(1:10,0.5)) 
 fitControl <- trainControl(method="repeatedcv",10,10,classProbs = T)
-fit_svmRadialCost <- train(result~., method='svmRadialCost', data=x_train,
-              trControl = fitControl, verbose=T, preProcess=c('pca'),
-              tuneGrid=Grid)
+fit_svmRadialCost <- train(result~V15+V13+V19+V35+V29+V40+V37+V33+V7+V24+V12+V5+V2+V21, 
+                           method='svmRadialCost', data=x,
+                           trControl = fitControl, verbose=T, preProcess=c('pca'),
+                           tuneGrid=Grid)
 Pred <- predict(fit_svmRadialCost, x_train)
 confusionMatrix(Pred, x_train$result)
+featureSelection <- varImp(fit_svmRadialCost)
+plot(featureSelection)
 
-##### Prediction #####
+##### Prediction Cross validation #####
 Pred <- predict(fit_svmRadialCost, x_test)
-confusionMatrix(Pred, x_test$result)
+confusionMatrix(Pred, x_test$result) ## acc 0.88 after 1st feature selection
+
+##### Prediction on Test datasets #####
+Pred_test <- predict(fit_svmRadialCost, test)
+Id <- c(1:length(Pred_test))
+submission <- data.frame(Id, Pred_test)
+names(submission)<-c("Id","Solution")
+write.table(submission, 'submission_svm_24Sep2014.csv',sep=',',row.names = F)
